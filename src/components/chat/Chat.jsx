d8468d701f4e963ba279c02dea9bc8a7e812b51f
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-01 09:10:17 +0200

    visually added user details

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './chat.css'
+import { UserContext } from './../../contexts/UserProvider';
 
 export const Chat = () => {
+
+   const { user } = useContext(UserContext);
+   console.log(user)
+
    return (
       <section id="chat">
          <div className="chat-container">
             <div className="friends-container">
-               <div className="user-info-container"></div>
+               <div className="user-info-container">
+                  {user
+                  ?
+                     <div className="user-details-wrapper">
+                        <img src={user?.avatarUrl} alt="user-avatar" />
+                        <p className='username'>{user?.username}</p>
+                     </div>
+                  :
+                     <p>Please login</p>
+                  }
+               </div>
             </div>
             <div className="messages-container"></div>
          </div>
